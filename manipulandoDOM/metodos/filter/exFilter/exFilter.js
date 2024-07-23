190003534d9fcfff9bd26dd4dfda51184bc2a0e5
Eduardo <eduardoshw123@gmail.com>
2024-07-23 11:02:47 -0300

    metodo remove()

--- manipulandoDOM/metodos/filter/exFilter/exFilter.js
+++ manipulandoDOM/metodos/filter/exFilter/exFilter.js
@@ -3,6 +3,7 @@ const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
 const cursos = ["HTML", "CSS", "javaScript", "PHP", "react", "MySQL", "reactNative"]
 const btnCursoSelecionado = document.querySelector("#btnCursoSelecionado")
+const btnRemoverCurso = document.querySelector("#btnRemoverCurso")
 
 cursos.map((elemento, chave)=>{
     const novoElemento = document.createElement("div")
@@ -22,14 +23,31 @@ cursos.map((elemento, chave)=>{
     caixaCursos.appendChild(novoElemento)
 })
 
-btnCursoSelecionado.addEventListener("click", ()=>{
+const radioSelecionado = ()=>{
     const todosRadios = [...document.querySelectorAll("input[name=rb_curso]")]
-    let radioSelecionado = todosRadios.filter((elemento)=>{
+    const radioSelecionado = todosRadios.filter((elemento)=>{
         return elemento.checked
     })
-    radioSelecionado = radioSelecionado[0]
+    return radioSelecionado[0]
+}
 
+btnCursoSelecionado.addEventListener("click", ()=>{
+    rs = radioSelecionado()
+    if(rs == undefined){
+        alert("Esolha um elemento")
+    }else{
     //const cursoSelecionado = radioSelecionado.parentNode.parentNode.firstChild.textContent
-    const cursoSelecionado = radioSelecionado.parentNode.previousSibling.textContent
+    const cursoSelecionado = rs.parentNode.previousSibling.textContent
     alert(`curso selecionado: ${cursoSelecionado}`)
+    }
+})
+
+btnRemoverCurso.addEventListener("click", ()=>{
+    rs = radioSelecionado()
+    if(rs == undefined){
+        alert("Escolha um elemento")
+    }else{
+    const cursoSelecionado = radioSelecionado().parentNode.parentNode
+    cursoSelecionado.remove()
+    }
 })
